bb9112e6dc0d94db12dd93bb04d2dcaab27800d4
peculiar <pecqliar2099@gmail.com>
2023-05-10 01:06:15 +0200

    modified some files

--- src/components/Footer/BottomLeftMenuItem.jsx
+++ src/components/Footer/BottomLeftMenuItem.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 
 export default function BottomLeftMenuItem(props) {
   return (
-    <div className='bottom-left-menu'>
+    <div>
         <a className='footer-link' href="#">
             {props.title}
         </a>
